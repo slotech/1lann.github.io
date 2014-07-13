f511ef5687cde46dddd170cddea15c1117fd26e3
Jason <1lanncontact@gmail.com>
2014-07-14 02:56:21 +0800

    More updates!

--- media-sync/communicate.js
+++ media-sync/communicate.js
@@ -1,4 +1,4 @@
-var peerJSKey = "t953urlnwv7ycik9";
+var peerJSKey = "esu07ebv3ni885mi";
 
 var isHost;
 var hostID;
@@ -7,19 +7,22 @@ var peerName;
 var peer;
 var hostConnection;
 
-var functionsForTypes = [];
-var connectedPeers = [];
+var timeoutTimeout;
 
-if (/^#(.{16})/.exec(window.location.hash) > 1) {
+var functionsForTypes = {};
+var connectedPeers = {};
+var nicknames = {};
+
+if (/^#(.+)/.exec(window.location.hash)) {
     isHost = false;
-    hostID = /^#(.{16})/.exec(window.location.hash);
+    hostID = /^#(.+)/.exec(window.location.hash)[1];
 } else {
     isHost = true;
 }
 
 
 var onMessageType = function(type, func) {
-    functionsForTypes[label] = func;
+    functionsForTypes[type] = func;
 };
 
 var clearMessageType = function(type) {
@@ -27,7 +30,16 @@ var clearMessageType = function(type) {
 };
 
 var sendData = function(to, type, data) {
-    
+    if (connectedPeers[to]) {
+        var targetPeer = connectedPeers[to];
+        targetPeer.send({
+            type: type,
+            data: data
+        });
+    } else {
+        console.log("No such target!");
+        return false;
+    }
 }
 
 // Override this
@@ -53,64 +65,126 @@ var receivedUnhandledData = function(from, type, data) {
 // Override end
 
 
-var registerPeerConnection = function(conn) {
-    connectedPeers[conn.label] = conn;
+var registerPeerConnection = function(conn, label) {
+    console.log("Registered new peer: "+conn.peer);
+    connectedPeers[conn.peer] = conn;
     
     conn.on("data", function(data) {
         if (data["type"] && data["data"]) {
             if (functionsForTypes[data["type"]]) {
-                functionsForTypes[data["type"]](self.label, data["data"]);
+                functionsForTypes[data["type"]](this.peer, data["data"]);
             } else {
-                receivedUnhandledData(self.label, data["type"], data["data"]);
+                receivedUnhandledData(this.peer, data["type"], data["data"]);
             }
-            receivedData(self.label, data["type"], data["data"]);
+            receivedData(this.peer, data["type"], data["data"]);
         }
     });
     
     conn.on("close", function() {
-        delete connectedPeers[self.label];
-        peerDisconnected(self.label);
+        delete connectedPeers[this.peer];
+        peerDisconnected(this.peer);
     });
     
     conn.on("error", function(err) {
-        peerError(self.label, err);
+        peerError(this.peer, err);
     });
+    
+    if (label) {
+        nicknames[conn.peer] = label;
+    }
         
-    peerConnected(conn.label);
+    peerConnected(conn.peer);
 }
 
 var connectToAllPeers = function(peerList) {
+    var nicknameBuffer = {};
     for (key in peerList) {
-        var connection = peer.connect(peerList[key], {label: peerName});
-        connection.on("open", function() {
-           registerPeerConnection(this); 
-        });
+        if (peerList[key] == hostID) {
+            nicknames[hostID] = key;
+        } else {
+            var connection = peer.connect(peerList[key]);
+            nicknameBuffer[peerList[key]] = key;
+            connection.on("open", function() {
+               registerPeerConnection(this, nicknameBuffer[this.peer]);
+            });
+
+            connection.on("error", function(err) {
+               peerError(this.peer, err); 
+            });
+        }
     }
 }
 
 var connect = function(name, callback) {
+    var connected = false;
+    
     peerName = name;
     peer = new Peer({key: peerJSKey});
+    console.log("Peer object created!");
     peer.on("open", function(id) {
+        console.log("Peer open and ready!");
         peerID = id;
         
         peer.on("connection", function(conn) {
-            registerPeerConnection(conn);
+            registerPeerConnection(conn, conn.label);
         });
         
         if (isHost) {
+            console.log("Hosting...")
             hostID = peerID;
             window.location.hash = peerID;
+            onMessageType("peer-list", function(from) {
+                console.log("Peer listing request from "+from);
+                var peerList = {};
+                for (key in connectedPeers) {
+                    if (key != from) {
+                        peerList[nicknames[key]] = key;
+                    }
+                }
+                peerList[peerName] = peerID;
+                sendData(from, "peer-list-response", peerList) 
+            });
             callback();
         } else {
-            hostConnection = peer.connect(hostID, {label: name});
+            console.log("Connecting to host...",hostID);
+            hostConnection = peer.connect(hostID, {label: peerName});
             hostConnection.on("open", function() {
+                connected = true;
                 console.log("Connected to host! Requesting peer list...")
                 registerPeerConnection(hostConnection);
-                hostConnection.send({type:"peer-list",content:"request"});
+                onMessageType("peer-list-response", function(from, data) {
+                    console.log("Receive peer-list")
+                    if (from == hostID) {
+                        clearMessageType("peer-list-response");
+                        connectToAllPeers(data);
+                    }
+                });
+                hostConnection.send({type:"peer-list",data:"request"});
                 callback();
             });
+            
+            timeoutTimeout = setTimeout(function() {
+                if (!connected) {
+                    console.log("Could not connect to host! Recreating as host...")
+                    isHost = true;
+                    connect(name, callback);
+                }
+            }, 5000);
+            
         }
         
     });
+    
+    peer.on("error", function(err) {
+        if (err.type == "peer-unavailable") {
+            clear(timeoutTimeout);
+            console.log("Error detected! Could not connect to host! Recreating as host...")
+            isHost = true;
+            connect(name, callback);
+        }
+    });
+}
+
+var quick = function(name) {
+    connect(name, function(){console.log("Connected!")});
 }
